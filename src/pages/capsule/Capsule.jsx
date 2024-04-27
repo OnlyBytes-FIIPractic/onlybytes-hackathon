c2fecd9a7d92baafe7be9a727c47392e41e07c4d
Tudor Stroescu <tudorstroescu@yahoo.com>
2024-04-27 23:57:03 +0300

    fixed bug

--- src/pages/capsule/Capsule.jsx
+++ src/pages/capsule/Capsule.jsx
@@ -116,13 +116,6 @@ const Capsule = () => {
                     >
                     </img>
                 </Card>
-                <div>
-                <Player
-      playsInline
-      poster="/assets/poster.png"
-      src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-    />
-                </div>
             </div>
         </>
     );
