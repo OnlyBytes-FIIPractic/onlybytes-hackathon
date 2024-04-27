93bc4c320d4f5b0b72136af40b9482fe327fa239
TiberiuBadelita <79211116+TiberiuBadelita@users.noreply.github.com>
2024-04-27 22:34:50 +0300

    Family Tree

--- src/pages/family/Family.jsx
+++ src/pages/family/Family.jsx
@@ -13,48 +13,60 @@ export function Family() {
         children: [
             {
                 name: "Father",
-                img1: "man1",
+                img1: "man3",
                 partner: "Mother",
-                img2: "woman1",
+                img2: "woman3",
                 children: [
                     {
                         name: "Child 1",
+                        img1: "woman1",
                         partner: "Child 1's partner",
+                        img2: "man1",
                         children: [
                             {
                                 name: "Grandchild 2",
+                                img1: "boy",
                                 children: []
                             }
                         ]
                     },
                     {
                         name: "Child 2",
+                        img1: "woman2",
                         partner: "Child 2's partner",
+                        img2: "man2",
                         children: []
                     }
                 ],
             },
             {
                 name: "Uncle",
-                img1: "man2",
+                img1: "woman5",
                 partner: "Aunt",
-                img2: "woman2",
+                img2: "woman6",
                 children: [
                     {
                         name: "Child 3",
+                        img1: "man12",
                         partner: "Child 3's partner",
+                        img2: "woman11",
                         children: []
                     },
                     {
                         name: "Child 4",
+                        img1: "woman12",
                         children: []
                     },
                     {
                         name: "Child 4",
+                        img1: "man21",
                         partner: "Child 4's partner",
+                        img2: "woman21",
                         children: [{
                             name: "Child 4",
+                            img1: "young1",
                             partner: "Child 4's partner",
+                            img2: "young2",
                             children: []
                         }
                         ]
@@ -76,7 +88,7 @@ export function Family() {
                             ))
                     )}
                 </Routes>
-                <div className="p-4">
+                <div className="p-4 ms-10">
                     <Tree root={mockTree} />
                 </div>
             </div>
